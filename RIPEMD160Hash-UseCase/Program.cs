42e83105d65c930cbc2f4d3e64abb5d1d076f884
Albion Ademi <albion.ademi@comitas.com>
2023-11-12 02:03:57 +0100

    Add project files.

---
+++ RIPEMD160Hash-UseCase/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+public class Program
+{
+    static void Main()
+    {
+        // Take input from the user
+        Console.Write("Enter a string to hash: ");
+        string inputString = Console.ReadLine();
+
+        // Convert the input string to a byte array
+        byte[] inputData = Encoding.UTF8.GetBytes(inputString);
+
+        // Compute RIPEMD-160 hash - TO DO
+    }
+}
